c09090ddf9db7cd9b2494783a7a748a882162d75
Vivek Kumar <www.vivekkumar0412@gmail.com>
2022-06-14 23:15:48 +0530

    keydown and keyup event

--- key.js
+++ key.js
@@ -1,5 +1,14 @@
 let inp = document.getElementById('inp');
 let p = document.getElementById('p');
+let p1 = document.getElementById('p');
 function keyPress(){
     p.innerHTML =` YOU HAVE PRESSED THE ${event.key}. `;
+};
+
+function keyDown(){
+    p1.innerHTML = `The key is pressed.`
+};
+
+function keyUp(){
+    p1.innerHTML = `The key is leaved.`
 };
